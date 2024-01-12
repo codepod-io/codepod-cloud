0123b18932d1eb46ebb8d5c6d9f5940fa2db85f9
Hebi Li <lihebi.com@gmail.com>
2024-01-11 16:38:34 -0800

    style toolbar with RadixUI; add color to toolbar

--- ui/src/components/nodes/rich/plugins/FloatingTextFormatToolbarPlugin/index.tsx
+++ ui/src/components/nodes/rich/plugins/FloatingTextFormatToolbarPlugin/index.tsx
@@ -27,14 +27,175 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import * as React from "react";
 import { createPortal } from "react-dom";
 
+import * as Toolbar from "@radix-ui/react-toolbar";
+import {
+  StrikethroughIcon,
+  TextAlignLeftIcon,
+  TextAlignCenterIcon,
+  TextAlignRightIcon,
+  FontBoldIcon,
+  FontItalicIcon,
+  CaretDownIcon,
+} from "@radix-ui/react-icons";
+
 import { getDOMRangeRect } from "../../utils/getDOMRangeRect";
 import { getSelectedNode } from "../../utils/getSelectedNode";
 import { setFloatingElemPosition } from "../../utils/setFloatingElemPosition";
 import DropdownColorPicker from "../../ui/DropdownColorPicker";
-import { $patchStyleText } from "@lexical/selection";
-// import {INSERT_INLINE_COMMAND} from '../CommentPlugin';
+import {
+  $getSelectionStyleValueForProperty,
+  $patchStyleText,
+} from "@lexical/selection";
+import { Code, Link, Underline } from "lucide-react";
+import { Button, Flex } from "@radix-ui/themes";
+import { DropdownMenu } from "@radix-ui/themes";
+import { match } from "ts-pattern";
 
-import { css } from "@emotion/react";
+function ColorPicker({ editor, fontColor, bgColor }) {
+  type Style = "red" | "green" | "blue" | "bg-yellow" | "bg-pink" | "none";
+  const style = match({ fontColor, bgColor })
+    .with({ fontColor: "red" }, () => "red")
+    .with({ fontColor: "green" }, () => "green")
+    .with({ fontColor: "blue" }, () => "blue")
+    .with({ bgColor: "yellow" }, () => "bg-yellow")
+    .with({ bgColor: "pink" }, () => "bg-pink")
+    .otherwise(() => "none") as Style;
+  const applyStyleText = useCallback(
+    (styles: Record<string, string>, skipHistoryStack?: boolean) => {
+      editor.update(
+        () => {
+          const selection = $getSelection();
+          if ($INTERNAL_isPointSelection(selection)) {
+            $patchStyleText(selection, styles);
+          }
+        },
+        skipHistoryStack ? { tag: "historic" } : {}
+      );
+    },
+    [editor]
+  );
+  const onFontColorSelect = useCallback(
+    (value: string, skipHistoryStack: boolean) => {
+      applyStyleText({ color: value }, skipHistoryStack);
+    },
+    [applyStyleText]
+  );
+  const onBgColorSelect = useCallback(
+    (value: string, skipHistoryStack: boolean) => {
+      applyStyleText({ "background-color": value }, skipHistoryStack);
+    },
+    [applyStyleText]
+  );
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <Button
+          variant="ghost"
+          className="hover:bg-violet3 hover:text-violet11 hover:border-emerald-400"
+          style={{
+            height: "0.7em",
+            color: fontColor,
+            backgroundColor: bgColor,
+          }}
+        >
+          A
+          <CaretDownIcon />
+        </Button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <Flex>
+          <DropdownMenu.Item
+            style={{
+              border: style === "none" ? "2px solid" : "",
+              padding: "5px",
+              height: "1.6em",
+            }}
+            onClick={() => {
+              onFontColorSelect("black", false);
+              onBgColorSelect("white", false);
+            }}
+          >
+            A
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            className={" text-red-500"}
+            style={{
+              border: style === "red" ? "2px solid" : "",
+              padding: "5px",
+              height: "1.6em",
+            }}
+            onClick={() => {
+              onFontColorSelect("red", false);
+              onBgColorSelect("white", false);
+            }}
+          >
+            A
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            className={" text-green-500"}
+            style={{
+              border: style === "green" ? "2px solid" : "",
+              padding: "5px",
+              height: "1.6em",
+              marginLeft: "1px",
+            }}
+            onClick={() => {
+              onFontColorSelect("green", false);
+              onBgColorSelect("white", false);
+            }}
+          >
+            A
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            className={" text-blue-500"}
+            style={{
+              border: style === "blue" ? "2px solid" : "",
+              padding: "5px",
+              height: "1.6em",
+              marginLeft: "1px",
+            }}
+            onClick={() => {
+              onFontColorSelect("blue", false);
+              onBgColorSelect("white", false);
+            }}
+          >
+            A
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            className={" bg-yellow-200"}
+            style={{
+              border: style === "bg-yellow" ? "2px solid" : "",
+              padding: "5px",
+              height: "1.6em",
+              marginLeft: "1px",
+            }}
+            onClick={() => {
+              onFontColorSelect("black", false);
+              onBgColorSelect("yellow", false);
+            }}
+          >
+            A
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            className={" bg-pink-200"}
+            style={{
+              border: style === "bg-pink" ? "2px solid" : "",
+              padding: "5px",
+              height: "1.6em",
+              marginLeft: "1px",
+            }}
+            onClick={() => {
+              onFontColorSelect("black", false);
+              onBgColorSelect("pink", false);
+            }}
+          >
+            A
+          </DropdownMenu.Item>
+        </Flex>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  );
+}
 
 function TextFormatFloatingToolbar({
   editor,
@@ -45,8 +206,8 @@ function TextFormatFloatingToolbar({
   isUnderline,
   isCode,
   isStrikethrough,
-  isSubscript,
-  isSuperscript,
+  fontColor,
+  bgColor,
 }: {
   editor: LexicalEditor;
   anchorElem: HTMLElement;
@@ -55,9 +216,9 @@ function TextFormatFloatingToolbar({
   isItalic: boolean;
   isLink: boolean;
   isStrikethrough: boolean;
-  isSubscript: boolean;
-  isSuperscript: boolean;
   isUnderline: boolean;
+  fontColor: string;
+  bgColor: string;
 }): JSX.Element {
   const popupCharStylesEditorRef = useRef<HTMLDivElement | null>(null);
 
@@ -183,12 +344,18 @@ function TextFormatFloatingToolbar({
     );
   }, [editor, updateTextFormatFloatingToolbar]);
 
+  const itemClasses1 = [
+    "flex-shrink-0 flex-grow-0 basis-auto ",
+    "text-mauve11 h-[25px] px-[5px] rounded inline-flex text-[13px] leading-none",
+    "items-center justify-center bg-white ml-0.5 outline-none",
+    "hover:bg-violet3 hover:text-violet11 focus:relative focus:shadow-[0_0_0_2px]",
+    "focus:shadow-violet7 first:ml-0",
+  ].join(" ");
+
   return (
     <div
       ref={popupCharStylesEditorRef}
-      className="floating-text-format-popup"
       style={{
-        display: "flex",
         background: "#fff",
         // padding: "4px",
         verticalAlign: "middle",
@@ -206,96 +373,114 @@ function TextFormatFloatingToolbar({
     >
       {editor.isEditable() && (
         <>
-          <button
-            type="button"
-            onClick={() => {
-              editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold");
-            }}
-            className={"popup-item spaced " + (isBold ? "active" : "")}
-            aria-label="Format text as bold"
+          <Toolbar.Root
+            className="flex px-[10px] py-[5px] w-full min-w-max rounded-md bg-white shadow-[0_2px_10px] shadow-blackA4"
+            aria-label="Formatting options"
           >
-            <i className="format bold" />
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic");
-            }}
-            className={"popup-item spaced " + (isItalic ? "active" : "")}
-            aria-label="Format text as italics"
-          >
-            <i className="format italic" />
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              editor.dispatchCommand(FORMAT_TEXT_COMMAND, "underline");
-            }}
-            className={"popup-item spaced " + (isUnderline ? "active" : "")}
-            aria-label="Format text to underlined"
-          >
-            <i className="format underline" />
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              editor.dispatchCommand(FORMAT_TEXT_COMMAND, "strikethrough");
-            }}
-            className={"popup-item spaced " + (isStrikethrough ? "active" : "")}
-            aria-label="Format text with a strikethrough"
-          >
-            <i className="format strikethrough" />
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              editor.dispatchCommand(FORMAT_TEXT_COMMAND, "subscript");
-            }}
-            className={"popup-item spaced " + (isSubscript ? "active" : "")}
-            title="Subscript"
-            aria-label="Format Subscript"
-          >
-            <i className="format subscript" />
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              editor.dispatchCommand(FORMAT_TEXT_COMMAND, "superscript");
-            }}
-            className={"popup-item spaced " + (isSuperscript ? "active" : "")}
-            title="Superscript"
-            aria-label="Format Superscript"
-          >
-            <i className="format superscript" />
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              editor.dispatchCommand(FORMAT_TEXT_COMMAND, "code");
-            }}
-            className={"popup-item spaced " + (isCode ? "active" : "")}
-            aria-label="Insert code block"
-          >
-            <i className="format code" />
-          </button>
-          <button
-            type="button"
-            onClick={insertLink}
-            className={"popup-item spaced " + (isLink ? "active" : "")}
-            aria-label="Insert link"
-          >
-            <i className="format link" />
-          </button>
+            <Toolbar.Button
+              onClick={() => {
+                editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold");
+              }}
+              className={[
+                itemClasses1,
+                isBold ? "text-violet11 bg-violet5" : "",
+              ].join(" ")}
+              value="bold"
+              aria-label="Bold"
+            >
+              <FontBoldIcon />
+            </Toolbar.Button>
+
+            {/* italic */}
+            <Toolbar.Button
+              onClick={() => {
+                editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic");
+              }}
+              className={[
+                itemClasses1,
+                isItalic ? "text-violet11 bg-violet5" : "",
+              ].join(" ")}
+              value="italic"
+              aria-label="Italic"
+            >
+              <FontItalicIcon />
+            </Toolbar.Button>
+
+            {/* underline */}
+            <Toolbar.Button
+              onClick={() => {
+                editor.dispatchCommand(FORMAT_TEXT_COMMAND, "underline");
+              }}
+              className={[
+                itemClasses1,
+                isUnderline ? "text-violet11 bg-violet5" : "",
+              ].join(" ")}
+              value="underline"
+              aria-label="Underline"
+            >
+              <Underline size={12} />
+            </Toolbar.Button>
+
+            {/* strikethrough */}
+            <Toolbar.Button
+              onClick={() => {
+                editor.dispatchCommand(FORMAT_TEXT_COMMAND, "strikethrough");
+              }}
+              className={[
+                itemClasses1,
+                isStrikethrough ? "text-violet11 bg-violet5" : "",
+              ].join(" ")}
+              value="strikethrough"
+              aria-label="Strike through"
+            >
+              <StrikethroughIcon />
+            </Toolbar.Button>
+
+            {/* code */}
+            <Toolbar.Button
+              onClick={() => {
+                editor.dispatchCommand(FORMAT_TEXT_COMMAND, "code");
+              }}
+              className={[
+                itemClasses1,
+                isCode ? "text-violet11 bg-violet5" : "",
+              ].join(" ")}
+              value="code"
+              aria-label="Insert code block"
+            >
+              <Code size={12} />
+            </Toolbar.Button>
+
+            {/* link */}
+            <Toolbar.Button
+              onClick={insertLink}
+              className={[
+                itemClasses1,
+                isLink ? "text-violet11 bg-violet5" : "",
+              ].join(" ")}
+              value="link"
+              aria-label="Insert link"
+            >
+              <Link size={12} />
+            </Toolbar.Button>
+
+            <Toolbar.Button
+              className={itemClasses1}
+              value="color"
+              aria-label="Font color"
+              style={{
+                padding: "10px",
+              }}
+            >
+              <ColorPicker
+                editor={editor}
+                fontColor={fontColor}
+                bgColor={bgColor}
+              />
+            </Toolbar.Button>
+          </Toolbar.Root>
         </>
       )}
-      <button
-        type="button"
-        // onClick={insertComment}
-        className={"popup-item spaced insert-comment"}
-        aria-label="Insert comment"
-      >
-        <i className="format add-comment" />
-      </button>
     </div>
   );
 }
@@ -310,9 +495,9 @@ function useFloatingTextFormatToolbar(
   const [isItalic, setIsItalic] = useState(false);
   const [isUnderline, setIsUnderline] = useState(false);
   const [isStrikethrough, setIsStrikethrough] = useState(false);
-  const [isSubscript, setIsSubscript] = useState(false);
-  const [isSuperscript, setIsSuperscript] = useState(false);
   const [isCode, setIsCode] = useState(false);
+  const [fontColor, setFontColor] = useState<string>("black");
+  const [bgColor, setBgColor] = useState<string>("white");
 
   const updatePopup = useCallback(() => {
     editor.getEditorState().read(() => {
@@ -345,9 +530,18 @@ function useFloatingTextFormatToolbar(
       setIsItalic(selection.hasFormat("italic"));
       setIsUnderline(selection.hasFormat("underline"));
       setIsStrikethrough(selection.hasFormat("strikethrough"));
-      setIsSubscript(selection.hasFormat("subscript"));
-      setIsSuperscript(selection.hasFormat("superscript"));
       setIsCode(selection.hasFormat("code"));
+
+      setFontColor(
+        $getSelectionStyleValueForProperty(selection, "color", "black")
+      );
+      setBgColor(
+        $getSelectionStyleValueForProperty(
+          selection,
+          "background-color",
+          "white"
+        )
+      );
 
       // Update links
       const parent = node.getParent();
@@ -407,71 +601,17 @@ function useFloatingTextFormatToolbar(
         isBold={isBold}
         isItalic={isItalic}
         isStrikethrough={isStrikethrough}
-        isSubscript={isSubscript}
-        isSuperscript={isSuperscript}
         isUnderline={isUnderline}
         isCode={isCode}
+        fontColor={fontColor}
+        bgColor={bgColor}
       />
+
       {/* <Toolbar2 /> */}
     </div>,
     anchorElem
   );
 }
-
-// function Toolbar2() {
-//   const [fontColor, setFontColor] = useState<string>("#000");
-//   const [bgColor, setBgColor] = useState<string>("#fff");
-//   const [editor] = useLexicalComposerContext();
-//   const [activeEditor, setActiveEditor] = useState(editor);
-//   const applyStyleText = useCallback(
-//     (styles: Record<string, string>, skipHistoryStack?: boolean) => {
-//       activeEditor.update(
-//         () => {
-//           const selection = $getSelection();
-//           if ($INTERNAL_isPointSelection(selection)) {
-//             $patchStyleText(selection, styles);
-//           }
-//         },
-//         skipHistoryStack ? { tag: "historic" } : {}
-//       );
-//     },
-//     [activeEditor]
-//   );
-//   const onFontColorSelect = useCallback(
-//     (value: string, skipHistoryStack: boolean) => {
-//       applyStyleText({ color: value }, skipHistoryStack);
-//     },
-//     [applyStyleText]
-//   );
-//   const onBgColorSelect = useCallback(
-//     (value: string, skipHistoryStack: boolean) => {
-//       applyStyleText({ "background-color": value }, skipHistoryStack);
-//     },
-//     [applyStyleText]
-//   );
-//   return (
-//     <div>
-//       <DropdownColorPicker
-//         // disabled={!isEditable}
-//         buttonClassName="toolbar-item color-picker"
-//         buttonAriaLabel="Formatting text color"
-//         buttonIconClassName="icon font-color"
-//         color={fontColor}
-//         onChange={onFontColorSelect}
-//         title="text color"
-//       />
-//       <DropdownColorPicker
-//         // disabled={!isEditable}
-//         buttonClassName="toolbar-item color-picker"
-//         buttonAriaLabel="Formatting background color"
-//         buttonIconClassName="icon bg-color"
-//         color={bgColor}
-//         onChange={onBgColorSelect}
-//         title="bg color"
-//       />
-//     </div>
-//   );
-// }
 
 export default function FloatingTextFormatToolbarPlugin({
   anchorElem = document.body,
