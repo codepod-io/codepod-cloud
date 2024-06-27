3fb1f2778d4bbde62a139afb64d9a0cc6c9a6b17
Hebi Li <lihebi.com@gmail.com>
2024-06-27 08:43:23 -0700

    chore: default copilot-manual-mode to true (#59)

--- ui/src/lib/store/settingSlice.ts
+++ ui/src/lib/store/settingSlice.ts
@@ -3,5 +3,5 @@ import { atom } from "jotai";
 export const ATOM_scopedVars = atom(true);
 export const ATOM_showAnnotations = atom(true);
 export const ATOM_devMode = atom(false);
-export const ATOM_copilotManualMode = atom(false);
+export const ATOM_copilotManualMode = atom(true);
 export const ATOM_showLineNumbers = atom(false);
