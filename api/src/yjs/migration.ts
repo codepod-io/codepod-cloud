7f4abee3a3da8f113fe447ec3e763ec5fe7ba0c2
Hebi Li <lihebi.com@gmail.com>
2024-07-24 03:35:57 +0000

    fix node hierarchy; fix code and result; replace scope with rich; run auto layout

--- api/src/yjs/migration.ts
+++ api/src/yjs/migration.ts
@@ -1,44 +1,76 @@
 import * as Y from "yjs";
+import { match, P } from "ts-pattern";
+import { flextree } from "d3-flextree";
 
 import prisma from "../prisma";
 import { json2yxml } from "./utils";
 import { PodResult } from "./types";
+import { assert } from "console";
+import { Node } from "reactflow";
+import { NodeData } from "@/../../ui/src/lib/store/types";
 
 /**
- * For historical reason, the backend DB schema pod.type are "CODE", "DECK",
- * "WYSIWYG", while the node types in front-end are "CODE", "SCOPE", "RICH".
+ * Auto layout.
  */
-
-function dbtype2nodetype(dbtype: string) {
-  switch (dbtype) {
-    case "CODE":
-      return "CODE";
-    case "DECK":
-      return "SCOPE";
-    case "WYSIWYG":
-      return "RICH";
-    default:
-      throw new Error(`unknown dbtype ${dbtype}`);
+export function layoutSubTree(nodesMap: Y.Map<Node<NodeData>>, id: string) {
+  // const data = subtree("1");
+  const rootNode = nodesMap.get(id);
+  if (!rootNode) throw new Error("Root node not found");
+  function subtree(id: string) {
+    const node = nodesMap.get(id);
+    if (!node) throw new Error("Node not found");
+    const children = node.data.children;
+    return {
+      id: node.id,
+      width: node.width!,
+      height: node.height!,
+      children: node.data.folded ? [] : children ? children.map(subtree) : [],
+    };
   }
-}
+  const data = subtree(id);
+  const paddingX = 100;
+  const paddingY = 100;
+  // const paddingX = 0;
+  // const paddingY = 0;
 
-function nodetype2dbtype(nodetype: string) {
-  switch (nodetype) {
-    case "CODE":
-      return "CODE";
-    case "SCOPE":
-      return "DECK";
-    case "RICH":
-      return "WYSIWYG";
-    default:
-      throw new Error(`unknown nodetype ${nodetype}`);
-  }
+  const layout = flextree({
+    children: (data) => data.children,
+    // horizontal
+    nodeSize: (node) => [
+      node.data.height + paddingY,
+      node.data.width + paddingX,
+    ],
+    // spacing: 100,
+  });
+  const tree = layout.hierarchy(data);
+  layout(tree);
+  // console.log("Layout Result", layout.dump(tree)); //=> prints the results
+  // tree.each((node) => console.log(`(${node.x}, ${node.y})`));
+  // update the nodesMap
+  tree.each((node) => {
+    const n = nodesMap.get(node.data.id)!;
+    // horizontal
+    nodesMap.set(node.data.id, {
+      ...n,
+      position: {
+        x: rootNode.position.x + node.y,
+        // center the node
+        y:
+          rootNode.position.y +
+          rootNode.height! / 2 +
+          node.x -
+          node.data.height / 2,
+        // y: node.x,
+      },
+    });
+  });
 }
 
 /**
  * Load content from the DB and migrate to the new Y.Doc format.
  * // TODO run the migration script seprately.
  */
+let count = 0;
 async function migrate_v_0_0_1(ydoc: Y.Doc, repoId: string) {
   // 1. query DB for repo.pods
   const repo = await prisma.repo.findFirst({
@@ -64,7 +96,7 @@ async function migrate_v_0_0_1(ydoc: Y.Doc, repoId: string) {
   // TODO make sure the ydoc is empty.
   // 2. construct Y doc types
   const rootMap = ydoc.getMap("rootMap");
-  const nodesMap = new Y.Map<any>();
+  const nodesMap = new Y.Map<Node<NodeData>>();
   const edgesMap = new Y.Map<any>();
   const codeMap = new Y.Map<Y.Text>();
   const richMap = new Y.Map<Y.XmlFragment>();
@@ -77,52 +109,109 @@ async function migrate_v_0_0_1(ydoc: Y.Doc, repoId: string) {
   const metaMap = new Y.Map();
   metaMap.set("version", "v0.0.1");
   rootMap.set("metaMap", metaMap);
+  // skip empty repo
+  if (repo.pods.length === 0) {
+    return;
+  }
+  // print progress
+  count++;
+  console.log(count, `migrating ${repo.id}`);
   // nodes
+  const node2children = new Map<string, string[]>();
+  repo.pods.forEach((pod) => {
+    const parent = pod.parent?.id || "ROOT";
+    const children = node2children.get(parent) || [];
+    // TODO order by geometry
+    children.push(pod.id);
+    node2children.set(parent, children);
+  });
   repo.pods.forEach((pod) => {
     nodesMap.set(pod.id, {
       id: pod.id,
-      type: dbtype2nodetype(pod.type),
+      type: match(pod.type)
+        .with("CODE", () => "CODE")
+        .with("DECK", () => "RICH")
+        .with("WYSIWYG", () => "RICH")
+        .with(P.union("MD", "REPL", "SCOPE"), () => {
+          throw new Error(`should not have dbtype ${pod.type}`);
+        })
+        .exhaustive(),
       data: {
         name: pod.name || undefined,
+        children: node2children.get(pod.id) || [],
         level: 0,
+        lang: pod.lang || "python",
+        folded: false,
       },
       position: {
         x: pod.x,
         y: pod.y,
       },
-      parentNode: pod.parent?.id,
       // TODO width & height
-      width: pod.width,
-      height: pod.height,
+      width: pod.type === "DECK" ? 300 : pod.width,
+      height: pod.type === "DECK" ? 100 : pod.height,
       style: {
-        width: pod.width,
-        height: pod.height,
+        width: pod.type === "DECK" ? 300 : pod.width,
       },
       dragHandle: ".custom-drag-handle",
     });
   });
-  // edges
-  repo.edges.forEach((edge) => {
-    edgesMap.set(`${edge.sourceId}_${edge.targetId}`, {
-      id: `${edge.sourceId}_${edge.targetId}`,
-      source: edge.sourceId,
-      target: edge.targetId,
-    });
+  assert(!nodesMap.has("ROOT"));
+  nodesMap.set("ROOT", {
+    id: "ROOT",
+    type: "RICH",
+    data: {
+      level: 0,
+      children: node2children.get("ROOT") || [],
+      folded: false,
+    },
+    position: { x: 0, y: 0 },
+    width: 300,
+    height: 100,
+    style: {
+      width: 300,
+    },
   });
+  richMap.set("ROOT", new Y.XmlFragment());
+
+  layoutSubTree(nodesMap, "ROOT");
+
+  // Edges are no longer used.
+  //
+  // repo.edges.forEach((edge) => {
+  //   edgesMap.set(`${edge.sourceId}_${edge.targetId}`, {
+  //     id: `${edge.sourceId}_${edge.targetId}`,
+  //     source: edge.sourceId,
+  //     target: edge.targetId,
+  //   });
+  // });
+
   // content
   repo.pods.forEach((pod) => {
     // let content : Y.Text | Y.XmlFragment;
     if (pod.type === "CODE") {
-      const content = new Y.Text(pod.content || undefined);
-      codeMap.set(pod.id, content);
+      // content
+      if (!pod.content) {
+        codeMap.set(pod.id, new Y.Text(""));
+      } else {
+        const str = JSON.parse(pod.content || "");
+        const content = new Y.Text(str || undefined);
+        codeMap.set(pod.id, content);
+      }
+      // result
       const data: any[] = [];
       if (pod.result) {
-        data.push({ type: "execute_result", text: pod.result });
+        const json = JSON.parse(pod.result);
+        if (pod.result) {
+          // data.push({ type: "execute_result", text: pod.result });
+          data.push({ type: "execute_result", ...json });
+        }
       }
+
       if (pod.stdout) {
         data.push({
           type: "stream_stdout",
-          text: pod.stdout,
+          text: JSON.parse(pod.stdout),
         });
       }
       // FIXME pod.error??
@@ -132,6 +221,28 @@ async function migrate_v_0_0_1(ydoc: Y.Doc, repoId: string) {
         const yxml = json2yxml(JSON.parse(pod.content));
         richMap.set(pod.id, yxml);
       }
+    } else if (pod.type === "DECK") {
+      // replace with Rich
+      if (pod.name) {
+        // put the scope name into the pod.
+        const json2 = {
+          type: "doc",
+          content: [
+            {
+              type: "heading",
+              attrs: { level: 2 },
+              content: [{ type: "text", text: pod.name }],
+            },
+            { type: "paragraph" },
+          ],
+        };
+        const yxml = json2yxml(json2);
+        richMap.set(pod.id, yxml);
+      } else {
+        richMap.set(pod.id, new Y.XmlFragment());
+      }
+    } else {
+      throw new Error(`unknown pod type ${pod.type}`);
     }
   });
 }
@@ -140,10 +251,7 @@ async function main() {
   await prisma.$transaction(
     async (tx) => {
       const repos = await tx.repo.findMany();
-      let count = 0;
       for (const repo of repos) {
-        count++;
-        console.log("Migrating repo", repo.id, count);
         const ydoc = new Y.Doc();
         await migrate_v_0_0_1(ydoc, repo.id);
         // now the ydoc should be populated with the content from the DB.
--- ui/src/lib/store/canvasSlice.ts
+++ ui/src/lib/store/canvasSlice.ts
@@ -136,11 +136,9 @@ export function updateView(get: Getter, set: Setter) {
   let selectedPods = get(ATOM_selectedPods);
   // let nodes = Array.from<Node>(nodesMap.values());
   // follow the tree order, skip folded nodes
-  function dfs(node: Node) {
+  function dfs(node: Node<NodeData>) {
     if (node.data.folded) return [node];
-    let children = node.data.children
-      .map((id) => nodesMap.get(id))
-      .filter((n) => n);
+    let children = node.data.children.map((id) => nodesMap.get(id)!);
     return [node, ...children.flatMap(dfs)];
   }
   let nodes = dfs(nodesMap.get("ROOT")!);
