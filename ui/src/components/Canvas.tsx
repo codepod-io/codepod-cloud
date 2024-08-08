7bded49a3d34d979db97f07bda7244bd6dec5657
Hebi Li <lihebi.com@gmail.com>
2024-08-08 21:39:00 +0000

    add comment

--- ui/src/components/Canvas.tsx
+++ ui/src/components/Canvas.tsx
@@ -231,8 +231,10 @@ function CanvasImpl() {
         deleteKeyCode={editMode === "view" ? null : "Backspace"}
         multiSelectionKeyCode={isMac ? "Meta" : "Control"}
         selectionMode={SelectionMode.Partial}
-        // TODO restore previous viewport
+        // Restore previous viewport.
         defaultViewport={{ zoom, x, y }}
+        // Center node on repo creation. INIT_ZOOM is a magic number (1.001) to
+        // trigger initial centering.
         fitView={zoom === INIT_ZOOM}
         fitViewOptions={{ maxZoom: 1 }}
         proOptions={{ hideAttribution: true }}
