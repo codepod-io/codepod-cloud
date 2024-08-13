6c7c944226291c61ed76e5093430db537307dae5
Hebi Li <lihebi.com@gmail.com>
2024-08-13 01:51:17 -0700

    use dropdown menu for user profile; selected return for /me api (#108)

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -2,43 +2,48 @@ import { Link as ReactLink, useLocation } from "react-router-dom";
 
 import { useNavigate } from "react-router-dom";
 
-import { Box, Button, Flex } from "@radix-ui/themes";
+import { Avatar, Box, DropdownMenu } from "@radix-ui/themes";
 
 import { trpc } from "@/lib/trpc";
 import { useAuth } from "@/lib/auth";
 
-const ProfileButton = () => {
-  const me = trpc.user.me.useQuery();
-  return (
-    <Box style={{ marginRight: 2 }}>
-      <ReactLink to="/profile">{me.data?.firstname}</ReactLink>
-    </Box>
-  );
-};
-
 export const UserProfile = () => {
   const { isSignedIn, signOut } = useAuth();
+  const me = trpc.user.me.useQuery();
   let navigate = useNavigate();
   return (
     <>
       {isSignedIn() ? (
-        <Flex
-          style={{
-            alignItems: "center",
-          }}
-          gap="4"
-        >
-          <ProfileButton />
-          <Button
-            onClick={() => {
-              signOut();
-              navigate("/login");
-            }}
-            variant="ghost"
-          >
-            Logout
-          </Button>
-        </Flex>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Box>
+              <Avatar
+                // src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+                // fallback="A"
+                fallback={
+                  me.data
+                    ? `${me.data?.firstname[0]}${me.data?.lastname[0]}`
+                    : "??"
+                }
+                radius="full"
+              />
+            </Box>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Item asChild>
+              <ReactLink to="/profile">Profile</ReactLink>
+            </DropdownMenu.Item>
+            <DropdownMenu.Separator />
+            <DropdownMenu.Item
+              onClick={() => {
+                signOut();
+                navigate("/login");
+              }}
+            >
+              Logout
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
       ) : (
         <Box display="block">
           <ReactLink to="/login">Login</ReactLink>
