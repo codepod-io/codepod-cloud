0929fdf8169b1ac3e0a0de75b8b99c0a9c68092f
Hebi Li <lihebi.com@gmail.com>
2024-08-13 09:25:07 +0000

    [chore] clean up deprecated code

--- ui/src/lib/store/settingSlice.ts
+++ ui/src/lib/store/settingSlice.ts
@@ -2,6 +2,5 @@ import { atom } from "jotai";
 
 export const ATOM_scopedVars = atom(true);
 export const ATOM_showAnnotations = atom(true);
-export const ATOM_devMode = atom(false);
 export const ATOM_copilotManualMode = atom(true);
 export const ATOM_showLineNumbers = atom(false);
