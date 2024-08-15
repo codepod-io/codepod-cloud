f72a641a9628dcec615cba3e408b6146ae701cc6
Hebi Li <lihebi.com@gmail.com>
2024-08-15 03:27:03 +0000

    do not call trpc.user.me when user is not signed in

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -25,7 +25,7 @@ function Banner() {
 }
 
 export function Header({ children }) {
-  console.log("env", env);
+  // console.log("env", env);
   return (
     <Flex
       direction="column"
@@ -58,46 +58,47 @@ export function Header({ children }) {
 
 export const UserProfile = () => {
   const { isSignedIn, signOut } = useAuth();
+  if (!isSignedIn()) {
+    return (
+      <Box display="block">
+        <ReactLink to="/login">Login</ReactLink>
+      </Box>
+    );
+  }
   const me = trpc.user.me.useQuery();
   let navigate = useNavigate();
   return (
     <>
-      {isSignedIn() ? (
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger>
-            <Box>
-              <Avatar
-                // src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-                // fallback="A"
-                fallback={
-                  me.data
-                    ? `${me.data?.firstname[0]}${me.data?.lastname[0]}`
-                    : "??"
-                }
-                radius="full"
-              />
-            </Box>
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content>
-            <DropdownMenu.Item asChild>
-              <ReactLink to="/profile">Profile</ReactLink>
-            </DropdownMenu.Item>
-            <DropdownMenu.Separator />
-            <DropdownMenu.Item
-              onClick={() => {
-                signOut();
-                navigate("/login");
-              }}
-            >
-              Logout
-            </DropdownMenu.Item>
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
-      ) : (
-        <Box display="block">
-          <ReactLink to="/login">Login</ReactLink>
-        </Box>
-      )}
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Box>
+            <Avatar
+              // src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+              // fallback="A"
+              fallback={
+                me.data
+                  ? `${me.data?.firstname[0]}${me.data?.lastname[0]}`
+                  : "??"
+              }
+              radius="full"
+            />
+          </Box>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Item asChild>
+            <ReactLink to="/profile">Profile</ReactLink>
+          </DropdownMenu.Item>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item
+            onClick={() => {
+              signOut();
+              navigate("/login");
+            }}
+          >
+            Logout
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
     </>
   );
 };
