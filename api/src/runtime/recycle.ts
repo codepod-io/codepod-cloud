4c06d37b9986d207e32cc2fc36e37c01f2faea41
Hebi Li <lihebi.com@gmail.com>
2024-08-26 12:28:19 -0700

    fix: kernel recycle time (#145)

--- api/src/runtime/recycle.ts
+++ api/src/runtime/recycle.ts
@@ -53,59 +53,89 @@ export async function registerKernelActivity(
   return true;
 }
 
+async function doRecycleKernel() {
+  console.log("recycling kernels ..");
+  const kernels = await prisma.kernel.findMany({
+    where: {
+      createdAt: {
+        // greater than 1 hr
+        // lt: new Date(Date.now() - 60 * 60 * 1000),
+        lt: new Date(Date.now() - kernelMaxLifetime),
+        //
+        // greater than 30s
+        // lt: new Date(Date.now() - 30 * 1000),
+      },
+    },
+  });
+  console.log("recycling kernels", kernels.length);
+  for (const kernel of kernels) {
+    // delete the k8s resources
+    console.log(`deleting rt-${kernel.repoId}-${kernel.name} ..`);
+    try {
+      await k8sAppsApi.deleteNamespacedDeployment(
+        `rt-${kernel.repoId}-${kernel.name}`,
+        env.RUNTIME_NS
+      );
+    } catch (e) {
+      if (e instanceof k8s.HttpError) {
+        console.error("error deleting deployment", e.body.reason);
+      } else {
+        throw e;
+      }
+    }
+
+    console.log(`deleting svc-${kernel.repoId}-${kernel.name} ..`);
+    try {
+      await k8sApi.deleteNamespacedService(
+        `svc-${kernel.repoId}-${kernel.name}`,
+        env.RUNTIME_NS
+      );
+    } catch (e) {
+      if (e instanceof k8s.HttpError) {
+        console.error("error deleting service", e.body.reason);
+      } else {
+        throw e;
+      }
+    }
+
+    console.log("clean up data structures ..");
+    // remove the zmq wire
+    const wireMap = repoId2wireMap.get(kernel.repoId);
+    const wire = wireMap?.get(kernel.name);
+    wire?.shell.close();
+    wire?.control.close();
+    wire?.iopub.close();
+    wireMap?.delete(kernel.name);
+    // TODO FIXME remove the ydoc
+    // set runtimeMap status to "stopped"
+    const ydoc = repoId2ydoc.get(kernel.repoId);
+    if (ydoc) {
+      const runtimeMap = ydoc
+        .getMap("rootMap")
+        .get("runtimeMap") as Y.Map<RuntimeInfo>;
+      runtimeMap.delete(kernel.name);
+    }
+
+    console.log("remove kernel entry from db ..");
+
+    // delete the kernel
+    await prisma.kernel.delete({
+      where: {
+        id: kernel.id,
+      },
+    });
+  }
+  console.log("recycling kernels done");
+}
+
 // a kernel cleaner thread that runs every 5 minutes to clean up kernels that are inactive for 1 hr.
 export function recycleKernels() {
+  console.log("recycle first time ..");
+  doRecycleKernel();
+  console.log("setting interval for recycling kernels ..");
   setInterval(
-    async () => {
-      console.log("recycling kernels ..");
-      const kernels = await prisma.kernel.findMany({
-        where: {
-          createdAt: {
-            // greater than 1 hr
-            // gt: new Date(Date.now() + 60 * 60 * 1000),
-            gt: new Date(Date.now() + kernelMaxLifetime),
-            //
-            // greater than 30s
-            // gt: new Date(Date.now() + 30 * 1000),
-          },
-        },
-      });
-      console.log("recycling kernels", kernels.length);
-      for (const kernel of kernels) {
-        // delete the k8s resources
-        await k8sAppsApi.deleteNamespacedDeployment(
-          `rt-${kernel.repoId}-${kernel.name}`,
-          env.RUNTIME_NS
-        );
-        await k8sApi.deleteNamespacedService(
-          `svc-${kernel.repoId}-${kernel.name}`,
-          env.RUNTIME_NS
-        );
-        // remove the zmq wire
-        const wireMap = repoId2wireMap.get(kernel.repoId);
-        const wire = wireMap?.get(kernel.name);
-        wire?.shell.close();
-        wire?.control.close();
-        wire?.iopub.close();
-        wireMap?.delete(kernel.name);
-        // TODO FIXME remove the ydoc
-        // set runtimeMap status to "stopped"
-        const ydoc = repoId2ydoc.get(kernel.repoId)!;
-        const runtimeMap = ydoc
-          .getMap("rootMap")
-          .get("runtimeMap") as Y.Map<RuntimeInfo>;
-        runtimeMap.delete(kernel.name);
-
-        // delete the kernel
-        await prisma.kernel.delete({
-          where: {
-            id: kernel.id,
-          },
-        });
-      }
-      console.log("recycling kernels done");
-    },
-    // 5 min
+    doRecycleKernel,
+    // every 5 min
     5 * 60 * 1000
     // 10s
     // 10 * 1000
