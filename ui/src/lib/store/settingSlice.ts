56072d3d27ebad9ba1c4b306fa468f5de5ee1d9b
Hebi Li <lihebi.com@gmail.com>
2024-09-17 14:07:50 -0700

    cleanup settings: remove scopedVars/annotation options; fix show linenumber (#207)

--- ui/src/lib/store/settingSlice.ts
+++ ui/src/lib/store/settingSlice.ts
@@ -1,6 +1,4 @@
 import { atom } from "jotai";
 
-export const ATOM_scopedVars = atom(true);
-export const ATOM_showAnnotations = atom(true);
 export const ATOM_copilotManualMode = atom(true);
 export const ATOM_showLineNumbers = atom(false);
