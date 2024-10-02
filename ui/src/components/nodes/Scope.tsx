e839a4b8143ce67ce49a457ffb3f70eb42ab1593
Hebi Li <lihebi.com@gmail.com>
2024-10-01 19:48:28 -0700

    add collision detection (#248)

--- ui/src/components/nodes/Scope.tsx
+++ ui/src/components/nodes/Scope.tsx
@@ -6,11 +6,16 @@ import * as Y from "yjs";
 import { Handle, Position, XYPosition } from "@xyflow/react";
 import { Box } from "@radix-ui/themes";
 import { myassert } from "@/lib/utils/utils";
+import { ATOM_collisionIds, ATOM_escapedIds } from "@/lib/store/canvasSlice";
 
 export function ScopeNode({ id }) {
   const nodesMap = useAtomValue(ATOM_nodesMap);
   const node = nodesMap.get(id);
   myassert(node);
+
+  // collisions
+  const collisionIds = useAtomValue(ATOM_collisionIds);
+  const escapedIds = useAtomValue(ATOM_escapedIds);
 
   return (
     <Box
@@ -19,6 +24,14 @@ export function ScopeNode({ id }) {
         opacity: 0.2,
         width: node.data.mywidth,
         height: node.data.myheight,
+        // styling
+        borderWidth: "5px",
+        borderStyle: "solid",
+        borderColor: escapedIds.includes(id)
+          ? "orange"
+          : collisionIds.includes(id)
+            ? "pink"
+            : "transparent",
       }}
     >
       Scope {id}
