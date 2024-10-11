20cc8b7e795037911a4391ce6f7331514852590e
Hebi Li <lihebi.com@gmail.com>
2024-10-11 17:29:19 +0000

    fix: resizable sidebar fold minwidth

--- ui/src/components/Sidebar_Tabs.tsx
+++ ui/src/components/Sidebar_Tabs.tsx
@@ -21,71 +21,54 @@ const MyTabsRoot = ({
   const [value, setValue] = useState(defaultValue);
   const [open, setOpen] = useState(true);
   return (
-    <Resizable
-      defaultSize={{
-        width: 320,
-      }}
-      minWidth={200}
-      enable={{
-        top: false,
-        right: side === "left",
-        bottom: false,
-        left: side === "right",
-        topRight: false,
-        bottomRight: false,
-        bottomLeft: false,
-        topLeft: false,
+    <Tabs.Root
+      orientation="vertical"
+      value={value}
+      style={{
+        display: "flex",
+        height: "100%",
+        flexDirection: "row",
+        // The sidebar tabs should be scrollable.
+        overflow: "scroll",
       }}
     >
-      <Tabs.Root
-        orientation="vertical"
-        value={value}
+      {open && side === "right" && children}
+      <Tabs.List
         style={{
-          display: "flex",
-          height: "100%",
-          flexDirection: "row",
-          // The sidebar tabs should be scrollable.
-          overflow: "scroll",
+          flexDirection: "column",
+          backgroundColor: "#eee",
+          border: "1px solid black",
+          alignItems: "flex-start",
+          zIndex: 2,
         }}
       >
-        {open && side === "right" && children}
-        <Tabs.List
-          style={{
-            flexDirection: "column",
-            backgroundColor: "#eee",
-            border: "1px solid black",
-            alignItems: "flex-start",
-            zIndex: 2,
-          }}
-        >
-          {tabs.map(({ key, icon }) => (
-            <Tabs.Trigger
-              key={key}
-              value={key}
-              style={{
-                ...(key === value ? { color: "red" } : {}),
-                justifyContent: "flex-start",
-              }}
-              onClick={(event) => {
-                event.preventDefault();
-                if (value === key) {
-                  setOpen(!open);
-                  // setValue("");
-                } else {
-                  setOpen(true);
-                  setValue(key);
-                }
-              }}
-            >
-              <RadixTooltip content={key} delayDuration={100} side="right">
-                {icon}
-              </RadixTooltip>
-            </Tabs.Trigger>
-          ))}
-        </Tabs.List>
-        {open && side === "left" && children}
-      </Tabs.Root>
-    </Resizable>
+        {tabs.map(({ key, icon }) => (
+          <Tabs.Trigger
+            key={key}
+            value={key}
+            style={{
+              ...(key === value ? { color: "red" } : {}),
+              justifyContent: "flex-start",
+            }}
+            onClick={(event) => {
+              event.preventDefault();
+              if (value === key) {
+                setOpen(!open);
+                // setValue("");
+              } else {
+                setOpen(true);
+                setValue(key);
+              }
+            }}
+          >
+            <RadixTooltip content={key} delayDuration={100} side="right">
+              {icon}
+            </RadixTooltip>
+          </Tabs.Trigger>
+        ))}
+      </Tabs.List>
+      {open && side === "left" && children}
+    </Tabs.Root>
   );
 };
 
@@ -100,30 +83,48 @@ export function MyTabs({
 }) {
   return (
     <MyTabsRoot tabs={tabs} side={side} defaultValue={defaultValue}>
-      <Box
-        style={{
-          border: "1px solid black",
-          width: "100%",
-          backgroundColor: gray.gray1,
-          // The sidebar panel should be scrollable.
-          overflow: "scroll",
-          padding: "1rem",
+      <Resizable
+        defaultSize={{
+          width: 320,
+        }}
+        minWidth={200}
+        enable={{
+          top: false,
+          right: side === "left",
+          bottom: false,
+          left: side === "right",
+          topRight: false,
+          bottomRight: false,
+          bottomLeft: false,
+          topLeft: false,
         }}
       >
-        <>
-          {tabs.map(({ key, content }) => (
-            <Tabs.Content
-              key={key}
-              value={key}
-              style={{
-                height: "100%",
-              }}
-            >
-              {content}
-            </Tabs.Content>
-          ))}
-        </>
-      </Box>
+        <Box
+          style={{
+            border: "1px solid black",
+            width: "100%",
+            height: "100%",
+            backgroundColor: gray.gray1,
+            // The sidebar panel should be scrollable.
+            overflow: "scroll",
+            padding: "1rem",
+          }}
+        >
+          <>
+            {tabs.map(({ key, content }) => (
+              <Tabs.Content
+                key={key}
+                value={key}
+                style={{
+                  height: "100%",
+                }}
+              >
+                {content}
+              </Tabs.Content>
+            ))}
+          </>
+        </Box>
+      </Resizable>
     </MyTabsRoot>
   );
 }
